ec572c44c56c066b77c71d1fc47fc67d30cb7246
NathanCLRY <nathan.charlery@gmail.com>
2024-07-08 11:39:58 -0400

    Configuration of model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,24 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'category_id',
+        'name',
+        'description',
+        'price',
+        'images',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
