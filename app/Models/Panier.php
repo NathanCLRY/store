7e738fa39d52696ea24061402379004a1401fb74
NathanCLRY <nathan.charlery@gmail.com>
2024-07-15 11:19:42 -0400

    Create some of the relations

--- app/Models/Panier.php
+++ app/Models/Panier.php
@@ -2,10 +2,42 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Product;
+use App\Models\Commande;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Panier extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'commande_id',
+        'product_id',
+        'quantite',
+    ];
+
+    /**
+     * Get the user that owns the Commande
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the commande that owns the Panier
+     *
+     * @return \Illuminate\Commandebase\Eloquent\Relations\BelongsTo
+     */
+    public function commande(): BelongsTo
+    {
+        return $this->belongsTo(Commande::class);
+    }
 }
