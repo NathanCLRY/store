aa47411b1ecfea37f35054b6e6285c3e637bd1f0
NathanCLRY <nathan.charlery@gmail.com>
2024-07-15 10:56:56 -0400

    create model and configure migration

--- app/Http/Controllers/PanierController.php
+++ app/Http/Controllers/PanierController.php
@@ -6,5 +6,16 @@ use Illuminate\Http\Request;
 
 class PanierController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'liste';
+    }
+    public function ajouter()
+    {
+        return 'ajouter';
+    }
+    public function commander()
+    {
+        return 'commander';
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PanierController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
@@ -20,6 +21,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+//Gestion du panier
+Route::middleware('auth')->group(function () {
+    Route::get('/panier', [PanierController::class, 'index'])->name('panier.lister');
+    Route::get('/panier/add/{product}', [PanierController::class, 'ajouter'])->name('panier.ajouter');
 });
 
 require __DIR__ . '/auth.php';
